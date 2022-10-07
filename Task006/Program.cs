f5f8f2e8491ad5b121904f0f134b076ebcd1709a
Rinat <rinsham55@gmail.com>
2022-10-07 23:50:55 +0300

    Add Task006

---
+++ Task006/Program.cs
@@ -0,0 +1,11 @@
+﻿// 6. Определить, является ли число чётным.
+
+int numberA = 4;
+string sStr = " ";
+if(numberA % 2 == 0){
+    sStr = "Число " + Convert.ToString(numberA) + " четное";
+}
+else{
+    sStr = "Число " + Convert.ToString(numberA) + " нечетное";
+}
+Console.WriteLine(sStr);
