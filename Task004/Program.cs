0e7acb47c534fd281c77d77533419a5baf9b2ae8
Rinat <rinsham55@gmail.com>
2022-10-07 23:48:22 +0300

    Add Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,14 @@
+﻿// 4. Найти максимальное из трех чисел
+
+int max = 0;
+int numberA = 2;
+int numberB = 3;
+int numberC = 7;
+if (numberA > numberB){
+    max = numberA;
+}
+else{
+    max = numberB;
+}
+if (max < numberC) max = numberC;
+Console.WriteLine("Максимальное число из трех = " + max);
