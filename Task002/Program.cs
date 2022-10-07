9a0ddda718d4106a85e6f4691ab825c22fdc2dc6
Rinat <rinsham55@gmail.com>
2022-10-07 23:22:32 +0300

    Initial commit. Add Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,16 @@
+﻿// 2. Даны два числа. Показать большее и меньшее число
+
+int max = 0;
+int min = 0;
+int numberA = 5;
+int numberB = 7;
+if (numberA > numberB){
+    max = numberA;
+    min = numberB;
+}
+else{
+    max = numberB;
+    min = numberA;
+}
+Console.WriteLine("Большее число = " + max);
+Console.WriteLine("Меньшее число = " + min);
