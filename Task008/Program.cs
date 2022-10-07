c22453d8e64d607cd059ee067e97e5c0557b7121
Rinat <rinsham55@gmail.com>
2022-10-07 23:52:25 +0300

    Add Task008

---
+++ Task008/Program.cs
@@ -0,0 +1,12 @@
+﻿// 8. Найти все чётные числа от 1 до N.
+
+int numberA = 1;
+Console.Write("Введите максмальное число = ");
+int max = Convert.ToInt32(Console.ReadLine());
+while(numberA <= max){
+    if (numberA % 2 == 0){
+        Console.Write(numberA + " ");
+    }  
+    numberA += 1;
+}
+Console.WriteLine("");
